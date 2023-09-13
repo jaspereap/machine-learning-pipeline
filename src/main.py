e1f6fa65e7e2543575dd86884ee72aba9a2830be
jaspereap <117494437+jaspereap@users.noreply.github.com>
2023-09-13 07:58:37 +0000

    General scoping of tasks to do

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+# Possible models for this project
+# Logistic Regression
+# Random Forest Classifier
+# Gradient Boosting Classifier
+
+# General to-do
+# Data preprocessing - decide how to handle missing values etc.
+# Split data into training and testing sets - do random selection
+# Evaluation metrics: accuracy, precision, recall, F1-score
