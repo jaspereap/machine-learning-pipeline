09c5ee8d29f608c87ab1a05989f5fc935d5733f3
Jaspereap <jaspereap@gmail.com>
2023-09-16 13:08:02 +0800

    Initial commit: Data module

---
+++ src/data.py
@@ -0,0 +1,20 @@
+# This Python module handles data retrieval and preprocessing
+
+import sqlite3
+import pandas as pd
+import yaml
+
+def load_config(config_path):
+    with open(config_path, 'r') as stream:
+        config = yaml.safe_load(stream)
+    return config
+
+def load_pre_purchase_data(config):
+    pre_purchase_db_path = config['data']['pre_purchase_db_path']
+    # Load data from the database and preprocess as needed
+    return pre_purchase_data
+
+def load_post_trip_data(config):
+    post_trip_db_path = config['data']['post_trip_db_path']
+    # Load data from the database and preprocess as needed
+    return post_trip_data
